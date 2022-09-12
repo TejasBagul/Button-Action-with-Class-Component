59d4007bfe1ba3a0d9f1522698ffb45334734fef
Tejas Bagul_07 <102860169+TejasBagul@users.noreply.github.com>
2022-09-12 10:23:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,26 @@
-import React, {Component} from "react"; 
- import '../styles/App.css'; 
-  
- class App extends Component { 
-     constructor(props) { 
-                 super(props); 
-                 this.state={ 
-                               id:"", 
-                         text:""
-                 } 
-         }; 
-  
-     render() { 
-                 const handleClick=()=>{ 
-                         this.setState({ 
-                                 id:'para', 
-                                 text:"Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy" 
-                         }) 
-                 } 
-             return( 
-                     <div id="main"> 
-                                 <p id={this.state.id}>{this.state.text}</p> 
-                                 <button id='click' onClick={handleClick}>Click</button> 
-                     </div> 
-             ); 
-     } 
- } 
-  
-  
- export default App;
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+
+class App extends Component {
+    constructor(props) {
+		super(props);
+	    this.state = {
+	    text: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
+		    click : 0
+	    
+	    }
+	};
+
+    render() {
+    	return(
+    		<div id="main">
+				{ /* Do not remove this main div!! */ }
+	<button id="click" onClick={() => this.setState({count: this.state.count + 1 })}>Button</button>
+    <h6 id="para">{this.state.text}</h6>
+    		</div>
+    	);
+    }
+}
+
+
+export default App;
